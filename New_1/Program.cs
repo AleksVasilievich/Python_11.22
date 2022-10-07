c73afcde0ad166fe738e21a3d9cdca20743db416
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-08 01:43:52 +0300

    add

---
+++ New_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число ->  ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write(a + b);
+}
+else
+{
+    Console.Write(a * b);
+}
+
+
+
+// Console.Write("Введите число ->");
+// int num = int.Parse(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length > 2)
+// {
+//     Console.Write(" третья цифра -> " + numText[2]);
+// }
+// else
+// {
+//     Console.Write(" -> третьей  цифры нет ");
+// }
