737197615330d0e46f3327421eb58ab85b4199c3
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-09 22:18:17 +0300

    add

---
+++ New_5/file_4.py
@@ -0,0 +1,29 @@
+# numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+# sam = 0
+# for nam in numbers:
+#     sam = sam + nam * nam
+# print(sam)
+
+
+
+
+
+# num = []
+# for i in range(int(input())):
+#     num.append(int(input()))
+# print(*num, sep='\n')
+# print()
+
+# for num1 in num:
+#     num1 = num1 * num1 + 2 * num1 + 1
+#     print(num1)
+
+
+
+# l = []
+# for i in range(int(input())):
+#     i = (int(input()))
+#     print(i)
+#     l.append(i**2+2*i+1)
+# print()
+# print(*l, sep='\n')
