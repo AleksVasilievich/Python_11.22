b932157a255dc027d6ee635d0775ca31fadbc232
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-15 14:07:32 +0300

    add

--- New_6-Task_2/new_1.py
+++ New_6-Task_2/new_1.py
@@ -5,9 +5,47 @@
 
 # print([i for i in range(int(input())) ])
 
-counter = 1
-n = int(input())
-ls = list(range(n))
-for i in ls:
-    counter *= i + 1
-    print(counter)
+# counter = 1
+# n = int(input())
+# ls = list(range(n))
+# for i in ls:
+#     counter *= i + 1
+#     print(counter)
+
+
+#  Задайте список. Напишите программу , которая определит, присутствует ли
+#  в заданном списке строкнекое число.
+
+# n = input()
+# flag = 0
+# ls = ['list 1', 'list 2', 'list 5']
+# for i in ls:
+#     if n in i:
+#         flag += 1
+# print('YES' if flag > 0 else 'NO')
+        
+#  Напишите прошрамму , которая определяет позицию второго вхождения 
+# строки в списке , либо сообщает что её нет.
+
+# count = 0
+# count1 = 0
+# ls = ['abc', 'qwe', 'zxc', 'qaz', 'abc', 'ert']
+# for i in ls:
+#     count1 += 1
+#     if i == 'abc':
+#         count += 1
+#         if count == 2:
+#             print(count1)
+# if count != 2:
+#     print('NO')
+
+
+#   Реализуйте алгоритм задания случайных чисел
+#  без встроенного генератора псевдослучайных чисел.
+
+# import time
+
+# t = 0
+# while t <= 50 or t > 100:
+#     t = int((time.time() - int(time.time())) * 1000)
+# print(t)
