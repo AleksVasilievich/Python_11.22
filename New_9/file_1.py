6aa0cd3760cd5d3a22c3344a12f0707ff9748b51
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-22 15:45:29 +0300

    add

---
+++ New_9/file_1.py
@@ -0,0 +1,20 @@
+# # Рекурсия : Нахождение чисел Фибоначи.
+
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+
+# ls = []
+# for e in range(1, 10):
+#     ls .append(fib(e))
+# print(ls)
+
+# ______________________________________________________
+
+def f(x):
+    return x * x
+
+ls1 = [1, 2, 3, 5, 8, 15, 23, 38]
+print(*[(i, f(i)) for i in ls1 if i % 2 == 0])
