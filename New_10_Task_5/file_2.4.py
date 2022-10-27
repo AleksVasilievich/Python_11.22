ea60074205f0e84ff2f13e8f0b47d84a711ece32
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-27 22:19:17 +0300

    add

---
+++ New_10_Task_5/file_2.4.py
@@ -0,0 +1,65 @@
+line = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+
+for i in line:
+    print(*i)
+
+pl_1 = '0'
+pl_2 = 'x'
+count = 0
+for n in range(9):
+    if n % 2 == 0:
+        print("1 игрок")
+        pl = pl_1
+
+        m = ''.join(filter(lambda x: x.isdigit(), input('Введите номер ячейки -> ')))
+        while m == '' or int(m) < 1 or int(m) > 9:
+            m = ''.join(filter(lambda x: x.isdigit(), input('Введите номер ячейки -> ')))
+        k_v = 0
+        k_s = 0
+        for i in line:
+            for j in i:
+                if j == m:
+                    k_v += int(j) - 1
+                    line[k_s][k_v] = pl
+            k_s += 1
+            k_v -= 3
+
+        for i in line:
+            print(*i)
+
+        if line[0][0] == line[0][1] == line[0][2] or line[1][0] == line[1][1] == line[1][2] or line[2][0] == line[2][1] == line[2][2]\
+                or line[0][0] == line[1][0] == line[2][0] or line[0][1] == line[1][2] == line[2][1] or line[0][2] == line[1][2] == line[2][2]\
+                or line[0][0] == line[1][1] == line[2][2] or line[0][2] == line[1][1] == line[2][0]:
+            print('Выиграл 1 игрок !')
+            count += 1
+            break
+
+    if n % 2 == 1:
+        print("2 игрок")
+        pl = pl_2
+
+        m = ''.join(filter(lambda x: x.isdigit(), input('Введите номер ячейки -> ')))
+        while m == '' or int(m) < 1 or int(m) > 9:
+            m = ''.join(filter(lambda x: x.isdigit(), input('Введите номер ячейки -> ')))
+        k_v = 0
+        k_s = 0
+        for i in line:
+            for j in i:
+                if j == m:
+                    k_v += int(j) - 1
+                    line[k_s][k_v] = pl
+            k_s += 1
+            k_v -= 3
+
+        for i in line:
+            print(*i)
+
+        if line[0][0] == line[0][1] == line[0][2] or line[1][0] == line[1][1] == line[1][2] or line[2][0] == line[2][1] == line[2][2]\
+                or line[0][0] == line[1][0] == line[2][0] or line[0][1] == line[1][2] == line[2][1] or line[0][2] == line[1][2] == line[2][2]\
+                    or line[0][0] == line[1][1] == line[2][2] or line[0][2] == line[1][1] == line[2][0]:
+            print('Выиграл 2 игрок !')
+            count += 1
+            break
+    if n == 8 and count == 0:
+        print('Ничья !')
+        
--- New_10_Task_5/file_3.py
+++ New_10_Task_5/file_3.py
@@ -2,3 +2,30 @@
 # stroka = "aaabbbbccbbb"
 # ....
 # stroka = "3a4b2c3b"
+################################################################################
+
+# stroka = ''.join(input().split())
+stroka = 'faaabbbbgccbbba'
+stroka_z = []
+count = 1
+n = 1
+
+for i in stroka[: -1]:
+    j = stroka[n]
+    if i == j:
+        count += 1
+        n += 1
+    else:
+        if count == 1:
+            stroka_z.append(i)
+            n += 1
+            count = 1
+        else:
+            stroka_z.append(str(count))
+            stroka_z.append(i)
+            n += 1
+            count = 1
+if i != j:
+    stroka_z.append(stroka[-1])
+    
+print(''.join(stroka_z))
