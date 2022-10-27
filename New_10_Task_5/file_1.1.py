e9c64128db65de44d7901d74cacbbe4b80234cdd
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-27 16:49:23 +0300

    add

--- New_10_Task_5/file_1.1.py
+++ New_10_Task_5/file_1.1.py
@@ -1,14 +1,13 @@
 from random import randint
 
 
-print('Игра с конфетами : всего 2021 конфета, 1 ход не более 28 , выигрывает сделавший последний ход')
-bank = 28
+print('Игра с конфетами : всего 2021 конфета, за 1 ход не более 28 , выигрывает сделавший последний ход')
+bank = 2021
 
 while bank > 0:
     nam = 28
     if bank < 28 and bank > 0:
         nam = bank
-    # player_1 = 1
     player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 1: ваш ход, введите число не более {nam} ->  ')))
     while player == '' or int(player) <= 0 or int(player) > nam:
         player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 1: ваш ход, введите число не более {nam} ->  ')))
@@ -16,16 +15,17 @@ while bank > 0:
 
     bank -= player_1
     print(f'Конфет в Банке осталось -> {bank} !')
-    nam = bank
     if bank <= 0:
         print('Выиграл Игрок 1')
         break
-
+    
+    nam = 28
+    if bank < 28 and bank > 0:
+        nam = bank
     player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 2: ваш ход, введите число не более {nam} ->  ')))
     while player == '' or int(player) <= 0 or int(player) > nam:
         player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 2: ваш ход, введите число не более {nam} ->  ')))
     player_2 = int(player)
-
 
     bank -= player_2
     print(f'Конфет в Банке осталось -> {bank} !')
@@ -33,11 +33,12 @@ while bank > 0:
         print('Выиграл Игрок 2')
         break                          
 
-    bank_1 = bank
-    bot = int(randint(1, bank + 1))   
+    nam = 28
+    if bank < 28 and bank > 0:
+        nam = bank
+    bot = int(randint(1, nam + 1))   
     bank -= bot
-    print(f'Игрок Бот, введите число не более {bank_1} -> {bot}')
+    print(f'Игрок Бот, введите число не более {nam} -> {bot}')
     print(f'Конфет в Банке осталось -> {bank} !')
     if bank <= 0:
         print('Выиграл Бот')
-
--- New_10_Task_5/file_1.py
+++ New_10_Task_5/file_1.py
@@ -15,7 +15,7 @@ from random import randint
 
 
 print('Игра с конфетами : всего 2021 конфета, 1 ход не более 28 , выигрывает сделавший последний ход')
-bank = 28
+bank = 202
 
 while bank > 0:
     nam = 28
@@ -41,13 +41,24 @@ while bank > 0:
     if bank <= 0:
         print('Выиграл Игрок 1')
         break
+    
+    nam = 28
+    if bank < 28 and bank > 0:
+        nam = bank
+    
+        # player_1 = 1
+    player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 2: ваш ход, введите число не более {nam} ->  ')))
+    while player == '' or int(player) <= 0 or int(player) > nam:
+        player = ''.join(filter(lambda x: x.isdigit(), input(f'Игрок 2: ваш ход, введите число не более {nam} ->  ')))
+    player_2 = int(player)
 
-    player_2 = int(input(f'Игрок 2: ваш ход, введите число не более {nam} ->  '))
-    while player_2 <= 0 or player_1 > nam:
-        player_2 = int(input(f'Игрок 2: ваш ход, введите число не более {nam} ->  '))
+    # player_2 = int(input(f'Игрок 2: ваш ход, введите число не более {nam} ->  '))
+    # while player_2 <= 0 or player_1 > nam:
+    #     player_2 = int(input(f'Игрок 2: ваш ход, введите число не более {nam} ->  '))
 
     bank -= player_2
     print(f'Конфет в Банке осталось -> {bank} !')
+    nam = bank
     if bank <= 0:
         print('Выиграл Игрок 2')
         break                           # нужен если участвует следующий игрок.
