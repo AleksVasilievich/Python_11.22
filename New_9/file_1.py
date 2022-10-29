88d507ce2fce7ebb1eef87be810f6aaeb2d9e326
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-10-30 00:21:03 +0300

    add

--- New_9/file_1.py
+++ New_9/file_1.py
@@ -24,4 +24,62 @@
 # print(data)
 
 
-print('YES' if input().endswith(('.com','.ru')) else 'NO')
+# print('YES' if input().endswith(('.com','.ru')) else 'NO')
+
+
+###########################################
+# ls = 'jfsdjlkpppppjf'
+# total = 0
+# ls_i = ''
+# for i in ls:
+#     if ls.count(i) >= total:
+#         total = ls.count(i)
+#         ls_i = i
+# print(ls_i)
+
+
+# ls = 'jfsdjlkpppppjf'[::-1]
+# print(max(ls, key = ls.count))
+
+# ###################################################     
+
+# ls = 'abcdeghabc'
+# n = ls.find('f')
+# m = ls.rfind('f')
+# if n != m:
+#     print(n, m, end='')
+# elif n == - 1:
+#     print('NO')
+# else:
+#     print(n)
+
+#########################
+
+# ls = 'ahbhchdeghahbhc'
+# n = ls.find('h')
+# m = ls.rfind('h')
+# ls_1 = ls[:n]
+# ls_2 = ls[m + 1:]
+# print(ls_1, end='' )
+# print(ls_2)
+# ______________________________
+# if n != m:
+#     print(n, m, end='')
+# elif n == - 1:
+#     print('NO')
+# else:
+#     print(n)
+
+
+
+# numbers = [8, 9, 10, 11]
+# nambers[1] = 17
+# # nambers.append(4)
+# # nambers.append(5)
+# # nambers.append(6)
+# # nambers.remove(8)
+# # nambers *= nambers
+# # nambers.append(25,3)
+# print(nambers)
+
+
