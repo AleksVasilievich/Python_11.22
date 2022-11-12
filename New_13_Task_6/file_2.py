b8e666831f3dd853e58bd70f04b749ef9a688f57
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-11-12 18:05:47 +0300

    add

--- New_13_Task_6/file_2.py
+++ New_13_Task_6/file_2.py
@@ -1,4 +1,19 @@
 # 2) Дан список, вывести отдельно буквы и цифры, пользуясь filter.
 #  [12,'sadf',5643] ---> ['sadf'] ,[12,5643]
 
-ls = list(filter(lambda i, type(i)= str))
+ls = [12,'sadf',5643]
+print(list(filter(lambda i: type(i) == str, ls)), list(filter(lambda i: type(i) != str, ls)))
+
+# def filters(ls):
+#     res_str = []
+#     res_float = []
+#     ls = [12,'sadf',5643]
+#     for i in ls:
+#         if type(i) == str:
+#             res_str.append(i)
+#         else:
+#             res_float.append(i)
+#     return res_str, res_float
+
+# lst = [12,'sadf',5643]
+# print(filters(lst))
