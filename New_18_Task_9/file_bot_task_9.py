095432257c08b829da464c6ebc8ba552a7ecc3ba
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-11-27 22:51:30 +0300

    add

---
+++ New_18_Task_9/file_bot_task_9.py
@@ -0,0 +1,28 @@
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+bot = Bot(token = '')                     # Создаём бота
+updater = Updater(token = '')     # Здесь будут хронится все обновления нашего чата,имя id и вся ифа.
+dispatcher = updater.dispatcher   # Переменная которая будет хранить всё что в updater 
+
+def start(update, context):        # Функция - (update и context - всегда ! ) 1 - инфа о чате, 2 - встроинные команды 
+    context.bot.send_message(update.effective_chat.id, "Hello")     # команда ответа бота
+
+def my_text(update, context):
+    text = update.message.text
+    if 'абв' in text.lower():
+        
+
+
+
+def text_filter(update, context):
+
+
+
+start_handler = CommandHandler("start", start)     # Если человек боту напишет "/start", то он запустит функцию старт
+                                                   # Функция выполница, бот ответит "Hello"
+                                                   
+dispahather.add_handler(start_handler)             # Обучаем бота добовленным фунциям, которые мы прописали ранее
+
+updater.start_polling()      # Старт программы
+updater.idle()               # Конец программы
